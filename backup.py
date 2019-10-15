8773bca7157660559e5118e9177a29cdd984fecf
aymar <aym@gm.com>
2019-10-15 15:00:21 +0100

    test cases for 'Yes', 'Y', 'y'

--- backup.py
+++ backup.py
@@ -1,3 +1,5 @@
+import re
+
 def backup(answer) :
     """
     >>> backup('Yes')
@@ -9,36 +11,48 @@ def backup(answer) :
     """
     return "Pass"
 
-def backup(answer) :
-    """
-    >>> backup('Ye')
-    'Fail'
-    >>> backup('No')
-    'Fail'
-    >>> backup('N')
-    'Fail'
-    >>> backup('n')
-    'Fail'
-    >>> backup('Maybe')
-    'Fail'
-    >>> backup(' ')
-    'Fail'
-    >>> backup("'")
-    'Fail'
-    >>> backup('"')
-    'Fail'
-    """
-    return "Fail"
+# def backup(answer) :
+#     """
+#
+#     >>> backup('No')
+#     'Fail'
+#     >>> backup('N')
+#     'Fail'
+#     >>> backup('n')
+#     'Fail'
+#     >>> backup('Maybe')
+#     'Fail'
+#     >>> backup(' ')
+#     'Fail'
+#     >>> backup("'")
+#     'Fail'
+#     >>> backup('"')
+#     'Fail'
+#     """
+#     return "Fail"
+#
+# def backup(answer) :
+#     """
+#     >>> backup('123')
+#     'Fail'
+#     >>> backup('ube123')
+#     'Fail'
+#     """
+#     if answer.isalnum() :
+#         return 'Fail'
 
-def backup(answer) :
-    """
-    >>> backup('123')
-    'Fail'
-    >>> backup('ube123')
-    'Fail'
-    """
-    if answer.isalnum() :
-        return 'Fail'
+# def backup(answer) :
+#     """
+#     >>> backup('123')
+#     'Fail'
+#     >>> backup('ube 123')
+#     'Fail'
+#     """
+#     x = re.search("\s", answer)
+#
+#     print(x)
+#
+# backup("The rain in Spain")
 
 if __name__ == "__main__":
     import doctest
